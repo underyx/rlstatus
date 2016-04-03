9e1e3b8082e29b6a2f03bdb2e8ca51e484c7b30d
Bence Nagy <bence@underyx.me>
2016-04-03 23:06:20 +0200

    Make worker loop infinitely

--- rlstatus/worker.py
+++ rlstatus/worker.py
@@ -1,19 +1,27 @@
 import os
 import sqlite3
+import logging
 
 from rlclient import RocketLeagueClient
 
 
 def main():
     client = RocketLeagueClient()
-    call = client.GetPopulationAllPlaylists()
-    results = client.request([call])[0]
-    rows = [(playlist, players) for playlist, players in results.items()]
+    calls = [client.GetPopulationAllPlaylists()]
+    while True:
+        try:
+            results = client.request(calls)[0]
+            rows = [(playlist, players) for playlist, players in results.items()]
 
-    with sqlite3.connect(os.environ['RLSTATUS_DB_URL']) as conn:
-        cur = conn.cursor()
-        cur.executemany('INSERT INTO population (playlist, players) VALUES (?, ?)', rows)
-        conn.commit()
+            with sqlite3.connect(os.environ['RLSTATUS_DB_URL']) as conn:
+                cur = conn.cursor()
+                LOG.info('saving %s rows into database', len(rows))
+                cur.executemany('INSERT INTO population (playlist, players) VALUES (?, ?)', rows)
+                conn.commit()
+        except:
+            LOG.exception('unhandled error')
+
+        LOG.info('population collection done, sleeping for %s seconds', int(os.getenv('RLSTATUS_CHECK_INTERVAL', '60')))
 
 if __name__ == '__main__':
     main()
