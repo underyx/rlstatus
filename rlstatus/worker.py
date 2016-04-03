69de5e0a4982bc70cf3eb31bfe4a127ccf954466
Bence Nagy <bence@underyx.me>
2016-04-03 21:02:22 +0200

    Let the DB take care of timestamping

--- rlstatus/worker.py
+++ rlstatus/worker.py
@@ -1,6 +1,5 @@
 import os
 import sqlite3
-from datetime import datetime
 
 from rlclient import RocketLeagueClient
 
@@ -9,15 +8,11 @@ def main():
     client = RocketLeagueClient()
     call = client.GetPopulationAllPlaylists()
     results = client.request([call])[0]
-    timestamp = datetime.now()
-    rows = [(timestamp, playlist, players) for playlist, players in results.items()]
+    rows = [(playlist, players) for playlist, players in results.items()]
 
     with sqlite3.connect(os.environ['RLSTATUS_DB_URL']) as conn:
         cur = conn.cursor()
-        cur.executemany(
-            'INSERT INTO population (timestamp, playlist, players) VALUES (?, ?, ?)',
-            rows,
-        )
+        cur.executemany('INSERT INTO population (playlist, players) VALUES (?, ?)', rows)
         conn.commit()
 
 if __name__ == '__main__':
